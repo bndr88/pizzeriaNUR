81e69cbdd6806d02ddd96bdeb846a3f7265ec4f4
Wender Mendez <wendermendez@gmail.com>
2024-11-17 01:22:28 -0400

    Update PersonalizadaFactory.php

--- Services/Pizzas/PersonalizadaFactory.php
+++ Services/Pizzas/PersonalizadaFactory.php
@@ -6,7 +6,7 @@ use Models\Pizza;
 use Services\Pizzas\PizzaFactory;
 
 class PersonalizadaFactory extends PizzaFactory {
-    public function crearPizza(array $ingredientes = [], float $precio ): Pizza {
+    public function crearPizza(array $ingredientes = [], float $precio =0): Pizza {
         $pizza = new Pizza('Personalizada',$ingredientes ,50.0);
         return $pizza;
     }
